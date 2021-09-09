7348a0533cc282c536018134ac65a57ce8b5e833
alibek.gao <alibek.gao@gmail.com>
2021-09-09 19:03:53 +0300

    check auth state

--- orbit-app/src/context/AuthContext.js
+++ orbit-app/src/context/AuthContext.js
@@ -30,6 +30,13 @@ const AuthProvider = ({ children }) => {
     })
   }
 
+  const isAuthenticated = () => {
+    if(!authState.token || !authState.expiresAt) {
+      return false;
+    }
+    return new Date().getTime() / 1000 < authState.expiresAt;
+  }
+
   return (
     <Provider
       value={{
